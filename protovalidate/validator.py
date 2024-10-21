8284e08b240ccb65e7c9de0342990f12dd90f6e0
Stefan VanBuren <svanburen@buf.build>
2024-10-21 16:59:51 -0400

    Remove unused import

--- protovalidate/validator.py
+++ protovalidate/validator.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import typing
-
 from google.protobuf import message
 
 from buf.validate import validate_pb2  # type: ignore
