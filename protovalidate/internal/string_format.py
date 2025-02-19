30cf42c3e2bbf0f9e4083d255cbda01a8233c6cf
Stefan VanBuren <svanburen@buf.build>
2025-02-19 09:36:39 -0500

    Skip type-checking; add link to issue to fix and test

--- protovalidate/internal/string_format.py
+++ protovalidate/internal/string_format.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# type: ignore
+# TODO(#257): Fully test and fix types in this file.
+
 from decimal import Decimal
 
 import celpy  # type: ignore
