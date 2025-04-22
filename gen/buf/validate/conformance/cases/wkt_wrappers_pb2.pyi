44885c118f7ee5568950ab7b6fb63186e062ce1d
Steve Ayers <smaye81@gmail.com>
2025-04-22 12:13:52 -0400

    Upgrade validation logic (#275)
    This includes upgrades to validation logic for:
    
    - URIs, URI references
    - IP addresses and prefixes (v4 and v6)
    - Hostname, Port
    - Email Address
    
    The validation here adheres to the defined RFC standards for each entity
    and passes conformance for all new conformance tests defined in:
    
    - https://github.com/bufbuild/protovalidate/pull/320
    - https://github.com/bufbuild/protovalidate/pull/341
    
    ---------
    
    Co-authored-by: Timo Stamm <ts@timostamm.de>
    Co-authored-by: Philip K. Warren <pkwarren@users.noreply.github.com>

--- gen/buf/validate/conformance/cases/wkt_wrappers_pb2.pyi
+++ gen/buf/validate/conformance/cases/wkt_wrappers_pb2.pyi
@@ -16,7 +16,8 @@ from buf.validate import validate_pb2 as _validate_pb2
 from google.protobuf import wrappers_pb2 as _wrappers_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
