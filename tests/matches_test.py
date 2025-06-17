4dd9232130ae7ff7553215823aa644952b5d7c24
Steve Ayers <smaye81@gmail.com>
2025-06-17 00:19:12 -0400

    Implement matches override (#319)
    This implements an override of the `matches` function in celpy, which
    allows us to perform quasi-re2 operations. Since google-re2 [does not
    yet support Python 3.13](https://github.com/google/re2/issues/516), we
    cannot use it in protovalidate-python.
    
    So, as a good compromise until then, we are simulating re2 behavior much
    like protovalidate-es does by failing on syntax not allowed in re2.
    
    Note that protovalidate-es also parses regex flags at the beginning of
    regex patterns and applies them at the end. This is because ECMAScript
    regex does not support flags at the beginning. However, Python's re
    package _does_ support this. So really all this does for now is to
    simply fail on invalid re2 syntax.
    
    A future PR will add the ability for users to specify their own re2
    engine via a config if they would like to override this behavior (which
    would not be a breaking change).
    
    ---------
    
    Signed-off-by: Sri Krishna <skrishna@buf.build>
    Co-authored-by: Sri Krishna <93153132+srikrsna-buf@users.noreply.github.com>

---
+++ tests/matches_test.py
@@ -0,0 +1,44 @@
+# Copyright 2023-2025 Buf Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import celpy
+from celpy import celtypes
+
+from protovalidate.internal import extra_func
+
+invalid_patterns = [
+    r"\1",
+    r"\k<name>",
+    r"Jack(?=Sprat)",
+    "Jack(?!Sprat)",
+    "(?<=Sprat)Jack",
+    "(?<!Sprat)Jack",
+    r"\cM\cJ",
+    r"\u0041",
+    r"\0 \01 \0a \012",
+    r"[\b]",
+]
+
+
+class TestMatches(unittest.TestCase):
+    def test_invalid_re2_syntax(self):
+        for pattern in invalid_patterns:
+            cel_pattern = celtypes.StringType(pattern)
+            try:
+                extra_func.cel_matches(celtypes.StringType("test"), cel_pattern)
+                self.fail(f"expected an error on pattern {cel_pattern}")
+            except celpy.CELEvalError as e:
+                self.assertEqual(str(e), f"error evaluating pattern {cel_pattern}, invalid RE2 syntax")
