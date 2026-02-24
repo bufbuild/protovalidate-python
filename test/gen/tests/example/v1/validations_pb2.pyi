452053905b9c530c1fe097a34666b419da848ad4
Anuraag Agrawal <anuraaga@gmail.com>
2026-02-24 14:21:58 +0900

    Ruff settings
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- test/gen/tests/example/v1/validations_pb2.pyi
+++ test/gen/tests/example/v1/validations_pb2.pyi
@@ -15,11 +15,15 @@
 from collections.abc import Iterable as _Iterable
 from collections.abc import Mapping as _Mapping
 from typing import ClassVar as _ClassVar
+from typing import Optional as _Optional
+from typing import Union as _Union
 
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf.internal import containers as _containers
+
+from buf.validate import validate_pb2 as _validate_pb2
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
